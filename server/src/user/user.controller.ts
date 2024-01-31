a9425fd1685371fc0c9a04947635beb120db44a0
Ddkotl <no64129@gmail.com>
2024-01-31 20:27:07 +0300

    user controller

--- server/src/user/user.controller.ts
+++ server/src/user/user.controller.ts
@@ -1,7 +1,42 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  Put,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import { Auth } from 'src/auth/decorators/auth.decorator';
+import { CurrentUser } from 'src/auth/decorators/user.decorator';
+import { UserDto } from './dto/user.dto';
 import { UserService } from './user.service';
 
-@Controller('user')
+@Controller('users')
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
+  @Get('profile')
+  @Auth()
+  async getProfile(@CurrentUser('id') id: number) {
+    return this.userService.byId(id);
+  }
+
+  @UsePipes(new ValidationPipe())
+  @HttpCode(200)
+  @Auth()
+  @Put('profile')
+  async updateProfile(@CurrentUser('id') id: number, @Body() dto: UserDto) {
+    return this.userService.updateProfile(id, dto);
+  }
+
+  // @HttpCode(200)
+  // @Auth()
+  // @Patch('profile/favorites/:postId')
+  // async toggleFavorite(
+  //   @Param('postId') postId: string,
+  //   @CurrentUser('id') id: number,
+  // ) {
+  //   return this.userService.toggleFavorite(id, postId);
+  // }
 }
--- server/src/user/user.service.ts
+++ server/src/user/user.service.ts
@@ -1,4 +1,41 @@
 import { Injectable } from '@nestjs/common';
+import { Prisma } from '@prisma/client';
+import { PrismaService } from 'src/prisma.service';
+import { UserDto } from './dto/user.dto';
+import { returnUserObject } from './return-user.object';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(private readonly prismaServise: PrismaService) {}
+  async byId(id: number, selectObject: Prisma.UserSelect = {}) {
+    const user = await this.prismaServise.user.findUnique({
+      where: { id },
+      select: {
+        ...returnUserObject,
+        favorites: {
+          select: {
+            id: true,
+            title: true,
+            images: true,
+            slug: true,
+            description: true,
+            content: true,
+          },
+        },
+        ...selectObject,
+      },
+    });
+
+    if (!user) {
+      throw new Error('Пользователь не найден');
+    }
+
+    return user;
+  }
+
+  async updateProfile(id: number, dto: UserDto) {
+    const isSameUser = await this.byId(id);
+
+    return updatedUser;
+  }
+}
