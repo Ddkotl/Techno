4fd01ce4f2df73106a9bccb59ca94445a0a7c84b
Ddkotl <no64129@gmail.com>
2024-02-08 12:08:07 +0300

    categry controllers

--- server/src/category/category.controller.ts
+++ server/src/category/category.controller.ts
@@ -1,5 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  Param,
+  Post,
+  Put,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import { Auth } from 'src/auth/decorators/auth.decorator';
 import { CategoryService } from './category.service';
+import { CategoryDto } from './dto/category.dto';
 
 @Controller('categories')
 export class CategoryController {
@@ -8,5 +21,39 @@ export class CategoryController {
   @Get()
   async getAll() {
     return this.categoryService.getAll();
+  }
+  @Get('by-slug/:slug')
+  async getBySlug(@Param('slug') slug: string) {
+    return this.categoryService.bySlug(slug);
+  }
+  @Get(':id')
+  @Auth()
+  async getById(@Param('id') id: string) {
+    return this.categoryService.byId(+id);
+  }
+
+  @HttpCode(200)
+  @Auth()
+  @Post()
+  async create() {
+    return this.categoryService.create();
+  }
+
+  @UsePipes(new ValidationPipe())
+  @HttpCode(200)
+  @Auth()
+  @Put(':categoryId')
+  async update(
+    @Param('categoryId') categoryId: string,
+    @Body() dto: CategoryDto,
+  ) {
+    return this.categoryService.update(+categoryId, dto);
+  }
+  @UsePipes(new ValidationPipe())
+  @HttpCode(200)
+  @Auth()
+  @Delete(':categoryId')
+  async delete(@Param('categoryId') categoryId: string) {
+    return this.categoryService.delete(+categoryId);
   }
 }
