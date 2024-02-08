3aeb54c5ecb18a7f2e6ea546091d430761232630
Ddkotl <no64129@gmail.com>
2024-02-08 13:03:15 +0300

    reviews

--- server/src/category/category.service.ts
+++ server/src/category/category.service.ts
@@ -1,4 +1,8 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { PrismaService } from 'src/prisma.service';
 import urlSlug from 'url-slug';
 import { CategoryDto } from './dto/category.dto';
@@ -17,7 +21,7 @@ export class CategoryService {
     });
 
     if (!category) {
-      throw new Error('Category not found');
+      throw new NotFoundException('Category not found');
     }
     return category;
   }
@@ -30,7 +34,7 @@ export class CategoryService {
     });
 
     if (!category) {
-      throw new Error('Category not found');
+      throw new NotFoundException('Category not found');
     }
     return category;
   }
--- server/src/review/review.service.ts
+++ server/src/review/review.service.ts
@@ -1,4 +1,43 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma.service';
+import { ReviewDto } from './dto/review.dto';
+import { returnReviewObject } from './return-review.object';
 
 @Injectable()
-export class ReviewService {}
+export class ReviewService {
+  constructor(private readonly prismaServise: PrismaService) {}
+
+  async getAll() {
+    return this.prismaServise.review.findMany({
+      orderBy: { createdAt: 'desc' },
+      select: returnReviewObject,
+    });
+  }
+
+  async create(userId: number, dto: ReviewDto, postId: number) {
+    return this.prismaServise.review.create({
+      data: {
+        ...dto,
+        post: {
+          connect: {
+            id: postId,
+          },
+        },
+        user: {
+          connect: {
+            id: userId,
+          },
+        },
+      },
+    });
+  }
+
+  async getAgregateValueByPostId(postId: number) {
+    return this.prismaServise.review
+      .aggregate({
+        where: { postId },
+        _avg: { rating: true },
+      })
+      .then((data) => data._avg);
+  }
+}
